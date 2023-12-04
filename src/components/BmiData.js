6f812431fb644df0acd6d348b87fb4d8d752024c
Salih <salihkm000@gmail.com>
2023-12-04 22:24:14 +0530

    Compleated

--- src/components/BmiData.js
+++ src/components/BmiData.js
@@ -1,18 +1,26 @@
-import React from 'react'
-
-function BmiData({bmiNo,bmiType,changeWeight}) {
-console.log(changeWeight);
+function BmiData({ bmiNo, bmiType, changeWeight }) {
+  console.log(changeWeight);
 
   return (
-    <div className='dataContainer'>
-        <h4>Your BMI Score</h4>
-        <h2>{bmiNo}</h2>
-        <h1>{bmiType}</h1>
-        {changeWeight.type === 'positive' && (<p className='text-danger'>"You need lose <b>{changeWeight.weight}</b>"</p>)}
-        {changeWeight.type === 'negative' && (<p className='text-warning'>"You need gain <b>{changeWeight.weight}</b>"</p>)}
-        {changeWeight.type === 'normal' && (<p className='text-success'>"Your weight is Normal,Keep it"</p>)}
+    <div className="dataContainer">
+      <h4>Your BMI Score</h4>
+      <h2>{bmiNo}</h2>
+      <h1>{bmiType}</h1>
+      {changeWeight.type === "positive" && (
+        <p className="text-danger">
+          "You need lose <b>{changeWeight.weight}</b>"
+        </p>
+      )}
+      {changeWeight.type === "negative" && (
+        <p className="text-warning">
+          "You need gain <b>{changeWeight.weight}</b>"
+        </p>
+      )}
+      {changeWeight.type === "normal" && (
+        <p className="text-success">"Your weight is Normal,Keep it"</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default BmiData
+export default BmiData;
